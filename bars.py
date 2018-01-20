9ba918b0d851c93196cb94d29fbf68a8f174cf07
Igor Grafin <apaticmail@gmail.com>
2018-01-20 22:46:33 +0300

    Initial commit

--- bars.py
+++ bars.py
@@ -1,73 +1,92 @@
 import json
 import sys
 from math import sqrt, pow
+import os
+import requests
+from geopy.distance import vincenty
 
 
 def load_data(file_path):
-    try:
-        with open(file_path, "r", encoding="UTF-8") as file:
-            json_data = json.load(file)
-            return json_data
-    except FileNotFoundError:
-        print("File %s not found!" % file_path)
-        return None
-    except json.decoder.JSONDecodeError:
-        print("JSON is not valid!")
+    with open(file_path, "r", encoding="UTF-8") as file:
+        json_data = json.load(file)
+        return json_data
+
+
+def get_data_from_api(api_key):
+    url = "https://apidata.mos.ru/v1/features/1796?api_key=" + api_key
+    request = requests.get(url)
+    if request.status_code == 200:
+        return request.json()
+    else:
         return None
 
 
 def get_biggest_bar(data):
     place_with_max_seat_count = data['features'][0]
-
     for place in data['features']:
-        if place["properties"]["Attributes"]["SeatsCount"] > place_with_max_seat_count["properties"]["Attributes"]["SeatsCount"]:
+        if place["properties"]["Attributes"]["SeatsCount"] > \
+                place_with_max_seat_count["properties"]["Attributes"]["SeatsCount"]:
             place_with_max_seat_count = place
 
-    print("Самый большой бар под названием {0} имеет сидячих мест {1}".format(
-        place_with_max_seat_count["properties"]["Attributes"]["Name"],
-        place_with_max_seat_count["properties"]["Attributes"]["SeatsCount"]))
+    return [place_with_max_seat_count["properties"]["Attributes"]["Name"],
+            place_with_max_seat_count["properties"]["Attributes"]["SeatsCount"]]
 
 
 def get_smallest_bar(data):
-    # print(data['features'])
-    # print(type(data['features']))
-    # print(data['features'][0])
-    # print(type(data['features'][0]))
-    # # print(data['features'][0]["properties"]["Attributes"]["Name"])
-    # print(data['features'][0]["properties"]["Attributes"]["SeatsCount"])
     place_with_min_seat_count = data['features'][0]
-
     for place in data['features']:
-        if place["properties"]["Attributes"]["SeatsCount"] < place_with_min_seat_count["properties"]["Attributes"]["SeatsCount"]:
+        if place["properties"]["Attributes"]["SeatsCount"] < \
+                place_with_min_seat_count["properties"]["Attributes"]["SeatsCount"]:
             place_with_min_seat_count = place
 
-    print("Самый маленький бар под названием {0} имеет сидячих мест {1}".format(
-        place_with_min_seat_count["properties"]["Attributes"]["Name"],
-        place_with_min_seat_count["properties"]["Attributes"]["SeatsCount"]))
+    return [place_with_min_seat_count["properties"]["Attributes"]["Name"],
+            place_with_min_seat_count["properties"]["Attributes"]["SeatsCount"]]
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(data, my_coord):
+    # get_distance = lambda x, y: sqrt(pow((x[0] - y[0]), 2) + pow((x[1] - y[1]), 2))
+    get_distance = lambda x, y: vincenty(x, reversed(y)).meters
     closest_place = data["features"][0]
-    # print(closest_place["geometry"]["coordinates"])
-    closest_place_coordinates = closest_place["geometry"]["coordinates"]
-    distance = sqrt(pow((longitude - closest_place_coordinates[0]), 2) +
-                    pow((latitude - closest_place_coordinates[1]), 2))
-# TODO: Разобраться с lambda
+
+    closest_distance = get_distance(my_coord, closest_place["geometry"]["coordinates"])
     for place in data['features']:
-        if place["geometry"]["coordinates"] < distance:
-            pass
+        current_distance = get_distance(my_coord, place["geometry"]["coordinates"])
+        if current_distance < closest_distance:
+            closest_place = place
+            closest_distance = current_distance
 
-    print(distance)
+    return closest_place["properties"]["Attributes"]["Name"],\
+           closest_place["properties"]["Attributes"]["Address"], closest_distance
 
 
-if __name__ == '__main__':
-    print(sys.argv)
-    if len(sys.argv) > 1:
-        json_data = load_data(sys.argv[1])
-        if json_data:
-            get_biggest_bar(json_data)
-            get_smallest_bar(json_data)
-            get_closest_bar(json_data, 55.600054, 37.718310)
-
+if __name__ == "__main__":
+    if os.getenv("API-mos-key"):
+        json_data = get_data_from_api(os.environ["API-mos-key"])
+        print("Загрузка онлайн")
+        if json_data is None:
+            print("неверный API key")
     else:
-        print(len(sys.argv))
+        try:
+            json_data = load_data(sys.argv[1])
+            print("Загрузка из файла")
+        except FileNotFoundError:
+            print("Файл %s не найден!" % sys.argv[1])
+            sys.exit(0)
+        except json.decoder.JSONDecodeError:
+            print("Некорректный JSON!")
+            sys.exit(0)
+        except IndexError:
+            print("Введите путь до файла с данными в качестве аргумента. Пример: 'python pprint_json.py in.json' ")
+            sys.exit(0)
+
+    if json_data:
+        print("Чтобы определить ближайший к вам бар, введите ваши координаты")
+        coordinates = (input("Введите координаты через запятую: ")).replace(" ", "")
+        biggest_bar = get_biggest_bar(json_data)
+        smallest_bar = get_smallest_bar(json_data)
+        closest_bar = get_closest_bar(json_data, coordinates)
+
+        print("Самый большой бар под названием {0} имеет сидячих мест {1}".format(biggest_bar[0],biggest_bar[1]))
+        print("Самый маленький бар под названием {0} имеет сидячих мест {1}".format(smallest_bar[0],smallest_bar[1]))
+        print("Ближайший к вам бар находится на расстоянии {0} метров под названием {1} находится по адресу {2}".format(
+            int(closest_bar[2]), closest_bar[0], closest_bar[1]))
